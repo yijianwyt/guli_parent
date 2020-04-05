c601ebe8662db909e5235e6559abb136e3523354
yijian941215 <879614047@qq.com>
2020-04-05 15:48:30 +0800

    guli

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -3,12 +3,16 @@ package com.atguigu.eduservice.controller;
 
 import com.atguigu.commonutils.R;
 import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.entity.vo.TeacherQuery;
 import com.atguigu.eduservice.service.EduTeacherService;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -42,5 +46,83 @@ public class EduTeacherController {
         boolean flag = teacherService.removeById(id);
             return flag;
     }
+    //3 分页查询讲师的方法
+    //current 当前页
+    //limit 每页记录数
+    @GetMapping("pageTeacher/{current}/{limit}")
+    public R pageListTeacher(@PathVariable long current,@PathVariable long limit){
+        Page<EduTeacher> eduTeacherPage=new Page<>(current,limit);
+        //调用方法实现分页
+        //调用方法时候，底层封装，把分页所有数据封装到pageTeacher对象里面
+        teacherService.page(eduTeacherPage,null);
+        long total=eduTeacherPage.getTotal();//总记录数
+        List<EduTeacher> records=eduTeacherPage.getRecords();
+        return R.ok().data("total",total).data("rows",records);
+    }
+    //4 条件查询带分页的方法
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageTeacherCondition(@PathVariable long current,@PathVariable long limit,
+                                  @RequestBody(required = false) TeacherQuery teacherQuery){
+        //创建page对象
+        Page<EduTeacher> eduTeacherPage=new Page<>(current,limit);
+        //构建条件
+        QueryWrapper<EduTeacher> wrapper=new QueryWrapper<>();
+        // 多条件组合查询
+        // mybatis学过 动态sql
+        String name =teacherQuery.getName();
+        Integer level =teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+        //判断条件值是否为空，如果不为空拼接条件
+        if(!StringUtils.isEmpty(name)) {
+            //构建条件
+            wrapper.like("name",name);
+        }
+        if(!StringUtils.isEmpty(level)) {
+            wrapper.eq("level",level);
+        }
+        if(!StringUtils.isEmpty(begin)) {
+            wrapper.ge("gmt_create",begin);
+        }
+        if(!StringUtils.isEmpty(end)) {
+            wrapper.le("gmt_create",end);
+        }
+        //调用方法实现条件查询分页
+        teacherService.page(eduTeacherPage,wrapper);
+
+        long total = eduTeacherPage.getTotal();//总记录数
+        List<EduTeacher> records = eduTeacherPage.getRecords(); //数据list集合
+        return R.ok().data("total",total).data("rows",records);
+    }
+    //添加讲师接口的方法
+    @PostMapping("addTeacher")
+    public R addTeacher(@RequestBody EduTeacher eduTeacher) {
+        boolean save = teacherService.save(eduTeacher);
+        if(save) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
+    }
+
+    //根据讲师id进行查询
+    @GetMapping("getTeacher/{id}")
+    public R getTeacher(@PathVariable String id) {
+        EduTeacher eduTeacher = teacherService.getById(id);
+        return R.ok().data("teacher",eduTeacher);
+    }
+
+    //讲师修改功能
+    @PostMapping("updateTeacher")
+    public R updateTeacher(@RequestBody EduTeacher eduTeacher) {
+        boolean flag = teacherService.updateById(eduTeacher);
+        if(flag) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
+    }
+
+
 }
 
